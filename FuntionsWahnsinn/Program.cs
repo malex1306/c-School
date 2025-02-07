57152c782e91653bc9ebc862a3be695786bbf466
malex1306 <alexandremarcel350@gmail.com>
2025-02-07 09:43:20 +0100

    new Projects

---
+++ FuntionsWahnsinn/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace FuntionsWahnsinn
+{
+
+    class Program
+    {
+        static int Funktion1(int x)
+        {
+            x += 1;
+            int y = Funktion2(x);
+            return x + y;
+        }
+
+        static int Funktion2(int x)
+        {
+            x += 2;
+            int y = Funktion3(x);
+            return x + y;
+        }
+
+        static int Funktion3(int x)
+        {
+            x += 3;
+            int y = Funktion4(x);
+            return x + y;
+        }
+
+        static int Funktion4(int x)
+        {
+            x += 4;
+            return x;
+        }
+
+        static void Main()
+        {
+            int x = 42;
+            int y = Funktion1(x);
+            int result = x + y;
+            Console.WriteLine(result); 
+        }
+    }
+}
---
+++ IntergerUmdrehen/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace IntergerUmdrehen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte geben sie eine Zahl ein!");
+            int zahl = Convert.ToInt32(Console.ReadLine());
+
+            int umgedrehteZahl = IntReverse(zahl);
+            Console.WriteLine($"Die umgedrehte zahl ist: {umgedrehteZahl}");
+        }
+
+
+        static int IntReverse(int zahl)
+        {
+            int umgekehrt = 0;
+            while (zahl != 0)
+            {
+                int ziffer = zahl % 10;
+                umgekehrt = umgekehrt * 10 + ziffer;
+                zahl /= 10;
+            }
+
+            return umgekehrt;
+        }
+    }
+}
