9651cef2bea2a9e912fdb512d3f02b6a9526a898
malex1306 <alexandremarcel350@gmail.com>
2025-02-14 09:00:45 +0100

    new projects

--- Brutto_Netto_Rechner/obj/Debug/net9.0/Brutto_Netto_Rechner.AssemblyInfo.cs
+++ Brutto_Netto_Rechner/obj/Debug/net9.0/Brutto_Netto_Rechner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Brutto_Netto_Rechner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a54a46682728433ace294ca82dfd45579e885e57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d20845ff6db6df696e2fd998e3ca8887c1777f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Brutto_Netto_Rechner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Brutto_Netto_Rechner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Muenzen/Program.cs
@@ -0,0 +1,45 @@
+﻿
+class Muenzwechsel
+{
+    static void Main()
+    {
+        Console.WriteLine("Geben Sie den Betrag ein, den Sie zurück bekommen: ");
+        int betrag = Convert.ToInt32(Console.ReadLine());
+        int[] geld = { 1, 2, 5, 10, 20, 50, 100, 200};
+        int[] minCoins = new int[betrag + 1]; // Array zur Speicherung der minimalen Münzen für jeden Betrag bis zum gegebenen Betrag
+        int[] lastCoin = new int[betrag + 1]; // Array zur Speicherung der letzten verwendeten Münze für jeden Betrag
+
+        // Initialisierung: Für Betrag 0 werden 0 Münzen benötigt
+        minCoins[0] = 0;
+
+        // Berechnung der minimalen Münzen für jeden Betrag von 1 bis amount
+        for (int i = 1; i <= betrag; i++)
+        {
+            minCoins[i] = int.MaxValue; // Setze den Wert zuerst auf ein sehr großes Integer
+
+            // Iteriere über alle Münzarten
+            for (int j = 0; j < geld.Length; j++)
+            {
+                // Überprüfe, ob die Münzart j verwendet werden kann
+                if (geld[j] <= i && minCoins[i - geld[j]] + 1 < minCoins[i])
+                {
+                    minCoins[i] = minCoins[i - geld[j]] + 1;
+                    lastCoin[i] = geld[j];
+                }
+            }
+        }
+
+        // Ausgabe der minimalen Anzahl von Münzen und der verwendeten Münzen
+        Console.WriteLine($"Minimale Anzahl von Münzen für den Betrag {betrag} Cent: {minCoins[betrag]}");
+
+        // Ausgabe der verwendeten Münzen
+        Console.Write("Verwendete Münzen: ");
+        int remaining = betrag;
+        while (remaining > 0)
+        {
+            Console.Write(lastCoin[remaining] + " ");
+            remaining -= lastCoin[remaining];
+        }
+
+    }
+}
